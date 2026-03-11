ea2777df8b3e0aa00ed8b4e7c1461a1306422fb0
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2026-03-11 22:53:13 +0700

    Update NurseScreen.tsx

--- MedTrackWeb/src/Nurse/NurseScreen.tsx
+++ MedTrackWeb/src/Nurse/NurseScreen.tsx
@@ -89,154 +89,106 @@ export default function NurseScreen() {
     }
 
     return (
-        <div className='mainBg h-100'>
-            <div className="container-fluid mt-5 pt-5">
-                <div className="row">
-                    <div className="col-lg-9 order-2 order-lg-1">
-
-                        <div className="row">
-                            {loading ? (
-                                <NurseInformation
-                                    nurseID={String(user?.nurseID)}
-                                    image={user?.image || ""}
-                                    fullName={user?.fullName || ""}
-                                    gender={
-                                        user?.gender == "1"
-                                            ? "Male"
-                                            : user?.gender == "2"
-                                                ? "Female"
-                                                : ""
-                                    }
-                                    dob={user?.dob?.split("T")[0] || ""}
-                                    phone={user?.phone || ""}
-                                    CIC={Number(user?.CIC)}
-                                    address={user?.address || ""}
-                                    email={user?.email || ""}
-                                    loading={loading}
-                                />
-                            ) : (
-                                <NurseInformation
-                                    nurseID={String(user?.nurseID)}
-                                    image={user?.image || ""}
-                                    fullName={user?.fullName || ""}
-                                    gender={
-                                        user?.gender == "1"
-                                            ? "Male"
-                                            : user?.gender == "2"
-                                                ? "Female"
-                                                : ""
-                                    }
-                                    dob={user?.dob?.split("T")[0] || ""}
-                                    phone={user?.phone || ""}
-                                    CIC={Number(user?.CIC)}
-                                    address={user?.address || ""}
-                                    email={user?.email || ""}
-                                    loading={loading}
-                                />
-                            )}
-                            <div className="col-lg-6 col-sm-12 ">
-                                <div className="hasSchedule padding border whiteBg marginBottom dropShadow">
-                                    <div className="row">
-                                        <div className="col-12 medicineSchedule padding50">
-                                            <h5 className='blueText medSche'>Medicine schedule</h5>
-                                            <div className="d-flex bd-highlight mb-3">
-                                                <p className='p-2 bd-highlight size50'>0</p>
-                                                <i className="ml-auto p-2 bd-highlight fa fa-bell-o blueText size50" aria-hidden="true"></i>
-                                            </div>
-                                            <a href="">More detail</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="hasSchedule padding border whiteBg dropShadow">
-                                    <div className="row medicineScheduleDetail">
-                                        <div className="col-lg-6 col-sm-6 d-flex justify-content-center mb-2">
-                                            <div className="border border-success square170-250 padding20 d-flex flex-column justify-content-between">
-                                                <h5 className="medSche greenText mb-3">Assigned Task</h5>
-                                                <div className="d-flex align-items-center mb-3">
-                                                    <p className="size25 greenText mb-0 me-auto">{count}</p>
-                                                    <i
-                                                        className="fa fa-calendar size25 greenText"
-                                                        aria-hidden="true"
-                                                        style={{ marginLeft: "auto" }}
-                                                    ></i>
-                                                </div>
-                                                <Link to="/home/schedule" className="greenText text-decoration-none">
-                                                    More detail
-                                                </Link>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-6 col-sm-6 d-flex justify-content-center ">
-                                            <div className="border border-info square170-250 padding20 d-flex flex-column justify-content-between">
-                                                <h5 className="medSche blueText mb-3">Patient's requirements</h5>
-                                                <div className="d-flex align-items-center mb-3">
-                                                    <p className="size25 blueText mb-0 me-auto">0</p>
-                                                    <i
-                                                        className="fa fa-calendar size25 blueText"
-                                                        aria-hidden="true"
-                                                        style={{ marginLeft: "auto" }}
-                                                    ></i>
-                                                </div>
-                                                <a href="#" className="blueText text-decoration-none">
-                                                    More detail
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+        <div className="row">
+            {loading ? (
+                <NurseInformation
+                    nurseID={String(user?.nurseID)}
+                    image={user?.image || ""}
+                    fullName={user?.fullName || ""}
+                    gender={
+                        user?.gender == "1"
+                            ? "Male"
+                            : user?.gender == "2"
+                                ? "Female"
+                                : ""
+                    }
+                    dob={user?.dob?.split("T")[0] || ""}
+                    phone={user?.phone || ""}
+                    CIC={Number(user?.CIC)}
+                    address={user?.address || ""}
+                    email={user?.email || ""}
+                    loading={loading}
+                />
+            ) : (
+                <NurseInformation
+                    nurseID={String(user?.nurseID)}
+                    image={user?.image || ""}
+                    fullName={user?.fullName || ""}
+                    gender={
+                        user?.gender == "1"
+                            ? "Male"
+                            : user?.gender == "2"
+                                ? "Female"
+                                : ""
+                    }
+                    dob={user?.dob?.split("T")[0] || ""}
+                    phone={user?.phone || ""}
+                    CIC={Number(user?.CIC)}
+                    address={user?.address || ""}
+                    email={user?.email || ""}
+                    loading={loading}
+                />
+            )}
+            <div className="col-lg-6 col-sm-12 ">
+                <div className="hasSchedule padding border whiteBg marginBottom dropShadow">
+                    <div className="row">
+                        <div className="col-12 medicineSchedule padding50">
+                            <h5 className='blueText medSche'>Medicine schedule</h5>
+                            <div className="d-flex bd-highlight mb-3">
+                                <p className='p-2 bd-highlight size50'>0</p>
+                                <i className="ml-auto p-2 bd-highlight fa fa-bell-o blueText size50" aria-hidden="true"></i>
                             </div>
-
-                            <div className="col-12 padding">
-                                <div className="hasRoomList border padding whiteBg dropShadow">
-                                    <h2 className='blueText text-center marginBottom'>Room list</h2>
-                                    <div>
-
-                                        <div className="row">
-                                            {rooms.map((room) => (
-                                                <Room key={room.roomID} {...room} />
-                                            ))}
-                                        </div>
-                                    </div>
+                            <a href="">More detail</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="hasSchedule padding border whiteBg dropShadow">
+                    <div className="row medicineScheduleDetail">
+                        <div className="col-lg-6 col-sm-6 d-flex justify-content-center mb-2">
+                            <div className="border border-success square170-250 padding20 d-flex flex-column justify-content-between">
+                                <h5 className="medSche greenText mb-3">Assigned Task</h5>
+                                <div className="d-flex align-items-center mb-3">
+                                    <p className="size25 greenText mb-0 me-auto">{count}</p>
+                                    <i
+                                        className="fa fa-calendar size25 greenText"
+                                        aria-hidden="true"
+                                        style={{ marginLeft: "auto" }}
+                                    ></i>
                                 </div>
+                                <Link to="/home/schedule" className="greenText text-decoration-none">
+                                    More detail
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-sm-6 d-flex justify-content-center ">
+                            <div className="border border-info square170-250 padding20 d-flex flex-column justify-content-between">
+                                <h5 className="medSche blueText mb-3">Patient's requirements</h5>
+                                <div className="d-flex align-items-center mb-3">
+                                    <p className="size25 blueText mb-0 me-auto">0</p>
+                                    <i
+                                        className="fa fa-calendar size25 blueText"
+                                        aria-hidden="true"
+                                        style={{ marginLeft: "auto" }}
+                                    ></i>
+                                </div>
+                                <a href="#" className="blueText text-decoration-none">
+                                    More detail
+                                </a>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-3 order-1 order-lg-2">
-                        <div className="leftBody border whiteBg marginBottom dropShadow">
-                            <div className="row">
-                                <div className="col-12 login">
+                </div>
+            </div>
 
-                                    <SidebarLogin
-                                        phone={user?.phone} fullName={user?.fullName}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="leftBody border whiteBg dropShadow marginBottom">
-                            <div className="row">
-                                <div className="col-12">
-                                    <h6 className='whiteText blueBg featureHead'>Feature</h6>
-                                    <div className="padding">
-                                        <ul className='list-unstyled'>
-                                            <li>
-                                                <Link to="/home/shift-change" className="text-decoration-none">
-                                                    <i className="fa fa-caret-right" aria-hidden="true"></i> Shift change registration
-                                                </Link>
-                                            </li>
-                                            <li>
-                                                <Link to="/home/daily-checking" className="text-decoration-none">
-                                                    <i className="fa fa-caret-right" aria-hidden="true"></i> Daily checking health
-                                                </Link>
-                                            </li>
-                                            <li>
-                                                <Link to="/home/schedule" className="text-decoration-none">
-                                                    <i className="fa fa-caret-right" aria-hidden="true"></i> Schedule
-                                                </Link>
-                                            </li>
-                                        </ul>
-                                    </div>
+            <div className="col-12 padding">
+                <div className="hasRoomList border padding whiteBg dropShadow">
+                    <h2 className='blueText text-center marginBottom'>Room list</h2>
+                    <div>
 
-                                </div>
-                            </div>
+                        <div className="row">
+                            {rooms.map((room) => (
+                                <Room key={room.roomID} {...room} />
+                            ))}
                         </div>
                     </div>
                 </div>
