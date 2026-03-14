ed15a716a6df0c30f6568854d543b0bce6eb773f
Hieuthao63d <125064888+Hieuthao63d@users.noreply.github.com>
2026-03-14 13:07:45 +0700

    Update DailyCheckingForm.tsx

--- MedTrackWeb/src/Nurse/DailyCheckingForm.tsx
+++ MedTrackWeb/src/Nurse/DailyCheckingForm.tsx
@@ -162,10 +162,10 @@ export default function DailyCheckingForm() {
 
   return (
     <div className=" radius10 shadow-sm mb-3">
-      <div className="p-3 radius10b0 blueBg text-white">
+      <div className="p-2 radius10b0 blueBg text-white">
         <h5 className="mb-0">Life function tracking sheet <i className="fa fa-file-text" /></h5>
       </div>
-      <div className="p-4 whiteBg dropShadow">
+      <div className="p-5 pt-3 whiteBg dropShadow">
         <ToastContainer />
         <h6 className="dlcgray">Update patient diagnostic indicators</h6>
         <form onSubmit={handleSubmit}>
