0b0527351e32f484d71bab816535d75bc6fa302d
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2026-03-15 15:53:04 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -26,10 +26,17 @@ app.use(cors());
 // Create MySQL connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  dateStrings: true,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
+  connectTimeout: 10000,
+  dateStrings: false,
   timezone: "+07:00",
 });
 
