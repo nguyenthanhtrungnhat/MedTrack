dd64f52657df68694fd796381d71c3c7455963a2
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2026-06-20 19:23:35 +0700

    Update api.ts

--- MedTrackWeb/src/api.ts
+++ MedTrackWeb/src/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000",
-    // baseURL: "https://projectb-medtrack.onrender.com",
+   // baseURL: "http://localhost:3000",
+   baseURL: "https://projectb-medtrack.onrender.com",
 });
 
 // REQUEST
